c37ba36a2edf056d8ccbbfd19ecfad2eab1c74f2
linn <linncoffeee@gmail.com>
2025-06-14 22:03:37 +0630

    updated how label UI works

--- files/static/js/helper.js
+++ files/static/js/helper.js
@@ -1,3 +1,11 @@
 function $(selector) {
     return document.querySelector(selector)
 }
+
+function myscroll(selector) {
+    $(selector).scrollIntoView({ block: 'start', behavior: 'smooth' })
+}
+
+function scrolltomain() {
+    myscroll("#main")
+}
